df1f3b88305851e1701f2654876dabc113894776
EdgarInfantes <21100749@ue.edu.pe>
2023-02-11 00:56:51 -0500

    Proyecto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+  styleUrls: ['./contacto.component.css'],
 })
 export class ContactoComponent {
-
+  envia(formu: NgForm) {
+    if (
+      formu.value.Nombre != '' &&
+      formu.value.Email != '' &&
+      formu.value.Telefono != '' &&
+      formu.value.mensaje != ''
+    ) {
+      alert('Se envió correctamente');
+      formu.reset();
+    } else {
+      alert('Complete todos los campos');
+    }
+  }
 }
